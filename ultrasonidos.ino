a2041c8e5312e4973fbb475eaf2ee9952950d79c
Unknown <franco.salinaspytel@gmail.com>
2018-04-28 11:58:30 +0200

    Estoy introduciendo mi archivo ultrasonidos.ino

---
+++ ultrasonidos.ino
@@ -0,0 +1,17 @@
+#include <Ultrasonic.h>
+
+int echo=A5;
+int trig=2;
+int distancia;
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+Ultrasonic ultrasonic(trig,echo);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+distancia=ultrasonic.distanceRead();
+Serial.println(distancia);
+}
